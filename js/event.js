bc2d5602d4d6af68ec3b799958fc1ce0e25d8db4
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-18 10:54:57 +0600

    text change completed

--- js/event.js
+++ js/event.js
@@ -1,9 +1,16 @@
 // console.log('connected');
+
 document.getElementById('update-btn').addEventListener('click', function () {
     // console.log('clicked')
 
     //to connect to any field or btn we should give an id
     let inputText = document.getElementById('input-field');
     let inputValue = inputText.value;
-    console.log(inputValue);
+    // console.log(inputValue);
+
+    //fetch the display field to display the text from input field
+
+    let textDisplayField = document.getElementById('textDisplayField');
+    textDisplayField.innerText = inputValue;
+    inputText.value = '';
 })
